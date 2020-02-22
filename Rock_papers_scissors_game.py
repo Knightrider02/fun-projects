dd2f96a6226b7cc249063cf23c0e0d2ca90248e6
Knightrider02 <38147148+Knightrider02@users.noreply.github.com>
2020-02-22 16:55:19 +0800

    Update Rock_papers_scissors_game.py

--- Rock_papers_scissors_game.py
+++ Rock_papers_scissors_game.py
@@ -1,23 +1,22 @@
 import random
-playing = "No"
-game_list = ["rock", "paper", "scissors"]
+playing = "Yes"
+game_list = ("rock", "paper", "scissors")
 while playing != "No":
-    print("This is a rock, paper and scissors game")
-    user_input = input("rock, papers or scissors ")
+    user_input = input("rock, paper or scissors ")
     computer_input = random.choice(game_list)
+    print(computer_input)
     if user_input == computer_input:
         print("It's  a tie")
-        if user_input == "rock" and computer_input == "paper":
-            print("Computer wins")
-            if user_input == "paper" and computer_input == "rock":
-                print("User wins")
-                if user_input == "rock" and computer_input == "scissors":
-                    print("User wins")
-                    if user_input == "scissors" and computer_input == "rock":
-                        print("Computer wins")
-                        if user_input == "scissors" and computer_input == "paper":
-                            print("User wins")
-                            if user_input == "paper" and computer_input == "scissors":
-                                print("Computer wins")
-                                playing = input("Do you want to continue playing, Yes or No")
-                                exit()
+    if user_input == "rock" and computer_input == "paper":
+        print("Computer wins")
+    if user_input == "paper" and computer_input == "rock":
+        print("User wins")
+    if user_input == "rock" and computer_input == "scissors":
+        print("User wins")
+    if user_input == "scissors" and computer_input == "rock":
+        print("Computer wins")
+    if user_input == "scissors" and computer_input == "paper":
+        print("User wins")
+    if user_input == "paper" and computer_input == "scissors":
+        print("Computer wins")
+    playing = input("Do you want to continue playing, Yes or No ")
