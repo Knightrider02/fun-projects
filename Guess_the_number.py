10b994f4cab6d08900db422daef4f1da1573ecb2
Knightrider02 <38147148+Knightrider02@users.noreply.github.com>
2020-04-30 08:57:59 +0800

    Update Guess_the_number.py

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -1,8 +1,8 @@
 import random
 playing = "Yes"
 answer = "Yes"
-number = random.randint(0, 21)
 while playing != "No":
+    number = random.randint(1, 21)
     guess = int(input("Guess a number from 0 to 20: "))
     if guess == number:
         print("You're correct")
@@ -10,10 +10,9 @@ while playing != "No":
         print("The number you guessed is too large")
     if guess < number:
         print("The number you guessed is too small")
-    if guess < 0 or guess > 20:
+    if guess < 1 or guess > 20:
         answer = input("Invalid number, would you like to try again? Yes or No: ")
         if answer == "Yes":
-            print("Goodnight, play tomorrow")
-            exit()
+            print("Welcome back")
         else:
             exit()
